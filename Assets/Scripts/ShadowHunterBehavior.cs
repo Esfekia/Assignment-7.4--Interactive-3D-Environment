ec96b80721c6198704fca5639a249fc426d6a1d4
Sertac <sertac@gmail.com>
2022-05-26 06:55:46 -0400

    main door animation sound added, shadow hunter prisoner sound trigger fixed
    main door animation sound added, shadow hunter prisoner sound trigger fixed

--- Assets/Scripts/ShadowHunterBehavior.cs
+++ Assets/Scripts/ShadowHunterBehavior.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class ShadowHunterBehavior : MonoBehaviour
 {
     
-    public AudioSource shadowHunterCurse;
+    public AudioSource shadowHunterKill;
     // OnTriggerEnter is called when the Collider other enters the trigger
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             
-            shadowHunterCurse.Play();
+            shadowHunterKill.Play();
 
         }
 
